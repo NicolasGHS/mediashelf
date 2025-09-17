39b3a18e3aefe9028a79d7eb843fdee7ca7b4514
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-09-17 22:35:50 +0200

    test component with moviecard

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,8 +1,11 @@
 <template>
-    <h1 class="text-2xl font-bold">Hello from Vue in Laravel 🎉</h1>
+    <h1 class="text-2xl font-bold">Movieshelf</h1>
+    <MovieCard />
 </template>
 
-<script setup></script>
+<script setup>
+import MovieCard from './components/MovieCard.vue';
+</script>
 
 <style scoped>
 h1 {
