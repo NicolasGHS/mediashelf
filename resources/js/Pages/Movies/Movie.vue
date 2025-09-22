540e12ea7ebee90ea379d86f6c32afe4adc6cfad
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-09-22 02:38:00 +0200

    show movie details

--- resources/js/Pages/Movies/Movie.vue
+++ resources/js/Pages/Movies/Movie.vue
@@ -1,8 +1,10 @@
 <script setup>
-    import { ref, onMounted } from "vue";
+    import MovieDetails from "@/components/MovieDetails.vue";
+import { ref, onMounted } from "vue";
 
     const movie = ref(null);
     const id = 132493;
+    const imagePath = "https://image.tmdb.org/t/p/w185";
 
     const fetchData = async () => {
         try {
@@ -17,10 +19,19 @@
     onMounted(fetchData);
 </script>
 <template>
-    <div v-if="movie">
-        <h1 class="text-2xl">{{ movie.original_title }}</h1>
-    </div>
-    <div v-else>
-        <p>Loading...</p>
+    <div class="items-center  w-full">
+        <div v-if="movie" class="flex items-center gap-8 w-full justify-center mt-10">
+            <img 
+                :src="`${imagePath}/${movie.poster_path}`" 
+            />
+            <div class="w-1/2">
+                <h1 class="text-2xl mb-2">{{ movie.original_title }}</h1>
+                <p>{{ movie.overview }}</p>
+            </div>
+        </div>
+        <div v-else>
+            <p>Loading...</p>
+        </div>
+        <MovieDetails :movieItem="movie" />
     </div>
 </template>
---
+++ resources/js/components/MovieDetails.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps(["movieItem"])
+</script>
+
+<template>
+    <div v-if="movieItem">
+        <div class="mb-3">
+            <p>From</p>
+            <ul class="flex gap-1">
+                <li v-for="country in movieItem.origin_country">
+                    <p>{{ country }}</p>
+                </li>
+            </ul>
+        </div>
+        <div class="mb-3">
+            <p>Language</p>
+            <p>{{ movieItem.original_language }}</p>
+        </div>
+        <div class="mb-3">
+            <p>Release Date</p>
+            <p>{{ movieItem.release_date }}</p>
+        </div>
+        <div class="mb-3">
+            <p>Duration</p>
+            <p>{{ movieItem.runtime }}</p>
+        </div>
+    </div> 
+</template>
